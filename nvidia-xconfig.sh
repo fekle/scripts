1e3f676203b85d3547740b7e5123c8f98a988a55
Felix Klein <felix@felixklein.com>
2020-02-19 17:48:45 +0100

    composite

--- nvidia-xconfig.sh
+++ nvidia-xconfig.sh
@@ -7,6 +7,6 @@ output="/etc/X11/xorg.conf"
 # sudo mv -fv "${output}" "${output}.nvidia-xconfig.bak"
 sudo nvidia-xconfig -o "${output}" \
   --cool-bits=28 --color-space=RGB --color-range=FULL -d 24 \
-  --composite --prime \
+  --composite --prime  \
   --force-composition-pipeline=FORCE-COMPOSITION-PIPELINE \
   --force-full-composition-pipeline=FORCE-FULL-COMPOSITION-PIPELINE
