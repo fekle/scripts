f700e0d2414cef2e28718fad5e7d9cde66d5048e
Felix Klein <felix@felixklein.com>
2020-02-20 19:01:05 +0100

    update gitsync

--- gitsync.sh
+++ gitsync.sh
@@ -15,24 +15,20 @@ repos=(
 )
 
 function gitsync_pull() {
-  (
-    set -euf
+  set -euf
 
-    # search for path, including replacing ~ with $HOME
-    path=$(realpath "${1//\~/${HOME}}")
-    cd "${path}"
+  # search for path, including replacing ~ with $HOME
+  path=$(realpath "${1//\~/${HOME}}")
+  printf "=> syncing '%s'\n" "${path}"
+  cd "${path}"
 
-    # pull and push
-    git add . && git diff-index --quiet HEAD && git pull && git push || { git add . && git commit -v && git pull && git push; }
-  ) || (
-    echo "=> updating ${1} failed"
-    exit 1
-  )
+  # pull and push
+  git add . >/dev/null && git diff-index --quiet HEAD >/dev/null && git pull && git push || { git add . && git commit -v && git pull && git push; }
+
+  printf "=> done syncing '%s'\n\n" "${path}"
 }
 export -f gitsync_pull
 
 for repo in "${repos[@]}"; do
- gitsync_pull $repo
+  gitsync_pull "${repo}"
 done
-
-#parallel 'gitsync_pull {}' ::: ${repos[@]}
