d59c70d42ea8295f4d51df1929f16c0544bab844
Felix Klein <felix@felixklein.com>
2020-02-20 18:38:40 +0100

    upddate nvidia-xconfig

--- nvidia-xconfig.sh
+++ nvidia-xconfig.sh
@@ -4,9 +4,15 @@ set -euf -o pipefail
 #input="/etc/X11/base-xorg.conf"
 output="/etc/X11/xorg.conf"
 
+NV_PRIME=""
+
+if s device felix-xps; then
+  NV_PRIME="--prime"
+fi
+
 # sudo mv -fv "${output}" "${output}.nvidia-xconfig.bak"
 sudo nvidia-xconfig -o "${output}" \
   --cool-bits=28 --color-space=RGB --color-range=FULL -d 24 \
-  --composite --prime  \
+  --composite ${NV_PRIME} \
   --force-composition-pipeline=FORCE-COMPOSITION-PIPELINE \
   --force-full-composition-pipeline=FORCE-FULL-COMPOSITION-PIPELINE
